3cea347ebf5231134388453c580a322a89d04c84
Koen Buitenhuis <161563@edu.nhtv.nl>
2018-11-20 15:07:37 +0100

    [CHANGE] Created material resource pool class [CHANGE] Created model resource pool class [CHANGE] Moved old resource pool functionality to new classes [CHANGE] Deleted old resource pool files [CHANGE] Created d3d12 material resource pool class [CHANGE] Created d3d12 model resource pool class [CHANGE] Moved old d3d12 resource pool functionality over to new classes [CHANGE] Deleted old d3d12 resource pool files

---
+++ src/d3d12/d3d12_resource_pool_material.cpp
@@ -0,0 +1,42 @@
+#include "d3d12_resource_pool_material.hpp"
+
+#include "d3d12_functions.hpp"
+#include "d3d12_renderer.hpp"
+
+namespace wr {
+	D3D12MaterialPool::D3D12MaterialPool(std::size_t size_in_mb) : MaterialPool(size_in_mb)
+	{
+	}
+
+	D3D12MaterialPool::~D3D12MaterialPool()
+	{
+	}
+
+	void D3D12MaterialPool::Evict()
+	{
+	}
+
+	void D3D12MaterialPool::MakeResident()
+	{
+	}
+
+	MaterialHandle D3D12MaterialPool::LoadMaterial(std::string_view path)
+	{
+		return MaterialHandle();
+	}
+
+	TextureHandle D3D12MaterialPool::LoadPNG(std::string_view path)
+	{
+		return TextureHandle();
+	}
+
+	TextureHandle D3D12MaterialPool::LoadDDS(std::string_view path)
+	{
+		return TextureHandle();
+	}
+
+	TextureHandle D3D12MaterialPool::LoadHDR(std::string_view path)
+	{
+		return TextureHandle();
+	}
+} /* wr */
