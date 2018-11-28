bdd5dcd6df3967b603b8985bf7f16076699cbf20
Jens Hagen <hagen-j@outlook.com>
2018-11-28 10:44:58 +0100

    [change] added correct compile flags [change] added line to run unit test

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,5 +1,6 @@
 #include "wisp.hpp"
 #include <gtest\gtest.h>
+
 int TestEntry()
 {
 	auto cl_string = GetCommandLineW();
@@ -7,6 +8,8 @@ int TestEntry()
 	wchar_t** cl_string_array = CommandLineToArgvW( cl_string, &argn );
 
 	::testing::InitGoogleTest( &argn, cl_string_array );
+	RUN_ALL_TESTS();
+
 
 	return 0;
 }
