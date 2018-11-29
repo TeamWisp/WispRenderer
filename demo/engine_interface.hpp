d7b61f8463c74db7d7e90a01350976497584e5d1
Viktor Zoutman <bideblade1@hotmail.com>
2018-11-29 15:44:46 +0100

    Fixed point and directional light calculations

---
+++ demo/engine_interface.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "wisp.hpp"
+#include "window.hpp"
+#include "imgui_tools.hpp"
+#include "scene_graph/scene_graph.hpp"
+#include "d3d12/d3d12_renderer.hpp"
+
+namespace engine
+{
+
+	static bool main_menu = true;
+	static bool open0 = true;
+	static bool open1 = true;
+	static bool open2 = true;
+	static bool open_console = false;
+	static bool show_imgui = true;
+	static bool fullscreen = false;
+	static char message_buffer[600];
+
+	static wr::imgui::special::DebugConsole debug_console;
+
+	void RenderEngine(wr::D3D12RenderSystem* render_system, wr::SceneGraph* sg)
+	{
+		debug_console.Draw("Console", &open_console);
+
+		if (!show_imgui) return;
+
+		if (main_menu && ImGui::BeginMainMenuBar())
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("Exit", "ALT+F4")) std::exit(0);
+				if (ImGui::MenuItem("Hide ImGui", "F1")) show_imgui = false;
+				ImGui::EndMenu();
+			}
+			if (ImGui::BeginMenu("Window"))
+			{
+				wr::imgui::menu::Registries();
+				ImGui::Separator();
+				ImGui::MenuItem("Theme", nullptr, &open0);
+				ImGui::MenuItem("ImGui Details", nullptr, &open1);
+				ImGui::MenuItem("Logging Example", nullptr, &open2);
+				ImGui::EndMenu();
+			}
+
+			ImGui::EndMainMenuBar();
+		}
+
+		ImGui::DockSpaceOverViewport(main_menu, nullptr, ImGuiDockNodeFlags_PassthruDockspace);
+
+		auto& io = ImGui::GetIO();
+
+		// Create dockable background
+		if (open0)
+		{
+			ImGui::Begin("Theme", &open0);
+			if (ImGui::Button("Cherry")) ImGui::StyleColorsCherry();
+			if (ImGui::Button("Unreal Engine")) ImGui::StyleColorsUE();
+			if (ImGui::Button("Light Green")) ImGui::StyleColorsLightGreen();
+			if (ImGui::Button("Light")) ImGui::StyleColorsLight();
+			if (ImGui::Button("Dark")) ImGui::StyleColorsDark();
+			if (ImGui::Button("Dark2")) ImGui::StyleColorsDarkCodz1();
+			ImGui::End();
+		}
+
+		if (open1)
+		{
+			ImGui::Begin("ImGui Details", &open1);
+			ImGui::Text("Mouse Pos: (%0.f, %0.f)", io.MousePos.x, io.MousePos.y);
+			ImGui::Text("Framerate: %.0f", io.Framerate);
+			ImGui::Text("Delta: %f", io.DeltaTime);
+			ImGui::Text("Display Size: (%.0f, %.0f)", io.DisplaySize.x, io.DisplaySize.y);
+			ImGui::End();
+		}
+
+		if (open2)
+		{
+			ImGui::Begin("Camera Settings", &open2);
+			
+			ImGui::DragFloat3("Position", sg->GetActiveCamera()->m_pos, 0.5f);
+			ImGui::DragFloat3("Rotation", sg->GetActiveCamera()->m_euler, 0.01f);
+
+			sg->GetActiveCamera()->SignalChange();
+
+			ImGui::End();
+		}
+
+		wr::imgui::window::ShaderRegistry();
+		wr::imgui::window::PipelineRegistry();
+		wr::imgui::window::RootSignatureRegistry();
+		wr::imgui::window::D3D12HardwareInfo(*render_system);
+		wr::imgui::window::D3D12Settings();
+	}
+	
+}
---
+++ demo/scene_cubes.hpp
@@ -0,0 +1,108 @@
+#pragma once
+
+#include "wisp.hpp"
+#include "window.hpp"
+#include "scene_graph/scene_graph.hpp"
+#include "resources.hpp"
+#include "imgui/imgui.hpp"
+
+namespace cubes_scene
+{
+
+	static std::vector<std::pair<std::shared_ptr<wr::MeshNode>, int>> bg_nodes(500);
+	static std::shared_ptr<wr::MeshNode> mesh_node;
+	static std::shared_ptr<wr::CameraNode> camera;
+	static std::shared_ptr<wr::LightNode> directional_light_node;
+	static float t = 0;
+
+	void CreateScene(wr::SceneGraph* scene_graph, wr::Window* window)
+	{
+		mesh_node = scene_graph->CreateChild<wr::MeshNode>(nullptr, resources::cube_model);
+		camera = scene_graph->CreateChild<wr::CameraNode>(nullptr, 70.f, (float)window->GetWidth() / (float)window->GetHeight());
+
+		// #### lights
+		auto point_light_node = scene_graph->CreateChild<wr::LightNode>(nullptr, wr::LightType::POINT, DirectX::XMVECTOR{ 1, 0, 0 });
+		point_light_node->SetPosition({ 0, 0, -6 });
+		point_light_node->SetRadius(5.f);
+
+		directional_light_node = scene_graph->CreateChild<wr::LightNode>(nullptr, wr::LightType::DIRECTIONAL, DirectX::XMVECTOR{ 1, 0, 0 });
+		directional_light_node->SetDirection({ 0, 0, 1 });
+
+		auto spot_light_node = scene_graph->CreateChild<wr::LightNode>(nullptr, wr::LightType::SPOT, DirectX::XMVECTOR{ 1, 1, 0 });
+		spot_light_node->SetPosition({ 0, 1, -6 });
+		spot_light_node->SetRadius(5.f);
+		spot_light_node->SetDirection({ 0, 0, 1 });
+		spot_light_node->SetAngle(40.f);
+
+		// #### background cubes
+		float distance = 20;
+		float cube_size = 2.5f;
+
+		float start_x = -30;
+		float start_y = -18;
+		float max_column_width = 30;
+
+		int column = 0;
+		int row = 0;
+
+		srand(time(0));
+		int rand_max = 15;
+
+		for (auto& node : bg_nodes)
+		{
+			node.first = scene_graph->CreateChild<wr::MeshNode>(nullptr, resources::cube_model);
+			node.first->SetPosition({ start_x + (cube_size * column), start_y + (cube_size * row), distance });
+			node.second = (rand() % rand_max + 0);
+
+			column++;
+			if (column > max_column_width) { column = 0; row++; }
+		}
+		// ### background cubes
+
+		camera->SetPosition(0, 0, -5);
+	}
+
+	void UpdateScene()
+	{
+		mesh_node->SetRotation({ sin(t / 2.f) * 20.f, -t * 10, 0 });
+
+		float perc = sin(time(0)) * 0.5f + 0.5f;
+
+		//If only you could write lerp({ 1, 0, 0 }, { 0, 1, 0 }, perc)
+		DirectX::XMVECTOR color = DirectX::XMVectorAdd(
+			DirectX::XMVectorMultiply({ 1, 0, 0 }, { perc, perc, perc }),
+			DirectX::XMVectorMultiply({ 0, 1, 0 }, { 1 - perc, 1 - perc, 1 - perc })
+		);
+
+		directional_light_node->SetColor(color);
+
+		for (auto& node : bg_nodes)
+		{
+			float speed = 16 + node.second;
+			switch (node.second)
+			{
+			case 0:	node.first->SetRotation({ t, 0, sin(t) * speed });	break;
+			case 1:	node.first->SetRotation({ t, 0, sin(-t) * speed }); break;
+			case 2: node.first->SetRotation({ -t, 0, sin(t) * speed });	break;
+			case 3: node.first->SetRotation({ -t, 0, sin(-t) * speed }); break;
+
+			case 4:	node.first->SetRotation({ t, sin(t) * speed, 0 });	break;
+			case 5:	node.first->SetRotation({ t, sin(-t) * speed, 0 }); break;
+			case 6: node.first->SetRotation({ -t, sin(t) * speed, 0 });	break;
+			case 7: node.first->SetRotation({ -t, sin(-t) * speed, 0 }); break;
+
+			case 8:	node.first->SetRotation({ sin(t) * speed, t, 0 });	break;
+			case 9:	node.first->SetRotation({ sin(-t) * speed, t, 0 }); break;
+			case 10: node.first->SetRotation({ sin(t) * speed, t, 0 });	break;
+			case 11: node.first->SetRotation({ sin(-t) * speed, t, 0 }); break;
+
+			case 12: node.first->SetRotation({ sin(t) * speed, 0, t });	break;
+			case 13: node.first->SetRotation({ sin(-t) * speed, 0, t }); break;
+			case 14: node.first->SetRotation({ sin(t) * speed, 0, -t }); break;
+			case 15: node.first->SetRotation({ sin(-t) * speed, 0, -t }); break;
+			}
+		}
+
+		t += 10.f * ImGui::GetIO().DeltaTime;
+	}
+} /* cube_scene */
