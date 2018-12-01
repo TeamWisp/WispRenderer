e6dd46b32b3706249448658f35beb405b2b11707
Viktor Zoutman <viktorzoutman@vzout.com>
2018-12-01 19:37:50 +0100

    Reduced code duplication, made the member variables private since the functions are final, replaced some for loops with ranged based for loops

--- src/d3d12/d3d12_model_pool.cpp
+++ src/d3d12/d3d12_model_pool.cpp
@@ -8,7 +8,7 @@ namespace wr
 {
 	namespace internal
 	{
-		inline std::uint64_t IndexFromBit(std::uint64_t frame) 
+		inline std::uint64_t IndexFromBit(std::uint64_t frame)
 		{
 			return frame / (8 * 8);
 		}
@@ -18,32 +18,119 @@ namespace wr
 			return frame % (8 * 8);
 		}
 
-		void SetPage(std::vector<uint64_t>* bitmap, std::uint64_t frame) 
+		void SetPage(std::vector<uint64_t> & bitmap, std::uint64_t frame)
 		{
-			std::uint64_t idx = IndexFromBit(frame);
-			std::uint64_t off = OffsetFromBit(frame);
-			bitmap->operator[](idx) |= (1Ui64 << off);
+			const std::uint64_t idx = IndexFromBit(frame);
+			const std::uint64_t off = OffsetFromBit(frame);
+			bitmap[idx] |= (1Ui64 << off);
 		}
 
-		void ClearPage(std::vector<uint64_t>* bitmap, std::uint64_t frame)
+		void ClearPage(std::vector<uint64_t> & bitmap, std::uint64_t frame)
 		{
-			std::uint64_t idx = IndexFromBit(frame);
-			std::uint64_t off = OffsetFromBit(frame);
-			bitmap->operator[](idx) &= ~(1Ui64 << off);
+			const std::uint64_t idx = IndexFromBit(frame);
+			const std::uint64_t off = OffsetFromBit(frame);
+			bitmap[idx] &= ~(1Ui64 << off);
 		}
 
-		bool TestPage(std::vector<uint64_t>* bitmap, std::uint64_t frame)
+		bool TestPage(std::vector<uint64_t> const & bitmap, std::uint64_t frame)
 		{
-			std::uint64_t idx = IndexFromBit(frame);
-			std::uint64_t off = OffsetFromBit(frame);
-			return (bitmap->operator[](idx) & (1Ui64 << off));
+			const std::uint64_t idx = IndexFromBit(frame);
+			const std::uint64_t off = OffsetFromBit(frame);
+
+			return (bitmap[idx] & (1Ui64 << off));
+		}
+
+		inline std::optional<std::uint64_t> FindFreePage(std::vector<std::uint64_t> const & bitmap, std::size_t const frame_count, std::uint64_t needed_frames)
+		{
+			bool counting = false;
+			bool found = false;
+
+			std::uint64_t start_frame = 0;
+			std::uint64_t free_frames = 0;
+
+			//Go through all all pages to find free ones.
+			//Because we're storing the data in a single bit we can compare an entire int64 at once to speed things up
+			for (std::uint64_t i = 0; i < bitmap.size(); ++i)
+			{
+				//Check 64 pages at once
+				if (bitmap[i] != 0Ui64)
+				{
+					//At least a single page is free, so check all 64 pages
+					for (std::uint64_t j = 0; j < 64; ++j)
+					{
+						//If the current page being checked is larger than the amount of available pages, break
+						if (i * 64 + j >= frame_count)
+						{
+							break;
+						}
+
+						//Set bit corresponding to page being checked in temporary variable
+						std::uint64_t to_test = 1Ui64 << j;
+
+						//Run an 'and' against the temporary variable to see if the page is available
+						if ((bitmap[i] & to_test))
+						{
+							//Is this the first free page found?
+							if (counting)
+							{
+								//Not the first free page, so increment the counter of consecutive free pages
+								free_frames++;
+								//Have we found the nessecary amount of free pages? If so, indicate that and break
+								if (free_frames == needed_frames)
+								{
+									found = true;
+									break;
+								}
+							}
+							else
+							{
+								//This is the first free page
+								if (needed_frames == 1)
+								{
+									//We only need a single page, so store the number of the page and break
+									found = true;
+									start_frame = i * 64 + j;
+									break;
+								}
+								else
+								{
+									//We need multiple pages, so store the number of this page as the start page and continue
+									start_frame = i * 64 + j;
+									counting = true;
+									free_frames = 1;
+								}
+							}
+						}
+						else
+						{
+							//The page wasn't free, so stop counting.
+							counting = 0;
+							free_frames = 0;
+						}
+					}
+				}
+				else
+				{
+					//All 64 pages are occupied, so stop counting
+					counting = false;
+					free_frames = 0;
+				}
+
+				//Have we found enough free pages? If so, break
+				if (found == true)
+				{
+					break;
+				}
+			}
+
+			return start_frame;
 		}
 	}
 
 	D3D12ModelPool::D3D12ModelPool(D3D12RenderSystem& render_system,
-		std::size_t vertex_buffer_size_in_mb, 
-		std::size_t index_buffer_size_in_mb) : 
-		ModelPool(vertex_buffer_size_in_mb, index_buffer_size_in_mb), 
+		std::size_t vertex_buffer_size_in_mb,
+		std::size_t index_buffer_size_in_mb) :
+		ModelPool(vertex_buffer_size_in_mb, index_buffer_size_in_mb),
 		m_render_system(render_system)
 	{
 		m_vertex_buffer = d3d12::CreateStagingBuffer(render_system.m_device,
@@ -67,38 +154,38 @@ namespace wr
 
 		m_vertex_buffer_bitmap.resize(page_frame_count);
 
-		for (int i = 0; i < m_vertex_buffer_bitmap.size() - 1; ++i)
+		for (auto& value : m_vertex_buffer_bitmap)
 		{
-			m_vertex_buffer_bitmap[i] = 0xffffffffffffffff;
+			value = 0xffffffffffffffff;
 		}
-		for (int i = 0; i < 64; ++i) 
+		for (int i = 0; i < 64; ++i)
 		{
-			if ((m_vertex_buffer_bitmap.size() - 1) * 64 + i < frame_count) 
+			if ((m_vertex_buffer_bitmap.size() - 1) * 64 + i < frame_count)
 			{
 				m_vertex_buffer_bitmap[m_vertex_buffer_bitmap.size() - 1] |= 1Ui64 << i;
 			}
-			else 
+			else
 			{
 				break;
 			}
 		}
-		
+
 		page_frame_count = SizeAlign(m_index_buffer_size / 65536, 64) / 64;
 		frame_count = m_index_buffer_size / 65536;
 
 		m_index_buffer_bitmap.resize(page_frame_count);
 
-		for (int i = 0; i < m_index_buffer_bitmap.size() - 1; ++i) 
+		for (auto& value : m_index_buffer_bitmap)
 		{
-			m_index_buffer_bitmap[i] = 0xffffffffffffffff;
+			value = 0xffffffffffffffff;
 		}
-		for (int i = 0; i < 64; ++i) 
+		for (int i = 0; i < 64; ++i)
 		{
 			if ((m_index_buffer_bitmap.size() - 1) * 64 + i < frame_count)
 			{
 				m_index_buffer_bitmap[m_index_buffer_bitmap.size() - 1] |= 1Ui64 << i;
 			}
-			else 
+			else
 			{
 				break;
 			}
@@ -110,8 +197,9 @@ namespace wr
 		d3d12::Destroy(m_vertex_buffer);
 		d3d12::Destroy(m_index_buffer);
 
-		for (int i = 0; i < m_mesh_handles.size(); ++i) {
-			delete m_mesh_handles[i];
+		for (auto& handle : m_mesh_handles)
+		{
+			delete handle;
 		}
 	}
 
@@ -175,161 +263,26 @@ namespace wr
 		//Get amount of needed memory pages
 		auto vertex_needed_frames = SizeAlign(num_vertices*vertex_size, 65536) / 65536;
 
-		std::uint64_t vertex_start_frame = 0;
-		bool counting = false;
-		bool found = false;
-		int free_frames = 0;
+		// Find Free Page
+		auto vertex_start_frame = internal::FindFreePage(m_vertex_buffer_bitmap, vertex_frame_count, vertex_needed_frames);
 
-		//Go through all all pages to find free ones.
-		//Because we're storing the data in a single bit we can compare an entire int64 at once to speed things up
-		for (std::uint64_t i = 0; i < m_vertex_buffer_bitmap.size(); ++i) 
-		{
-			//Check 64 pages at once
-			if (m_vertex_buffer_bitmap[i] != 0Ui64)
-			{
-				//At least a single page is free, so check all 64 pages
-				for (std::uint64_t j = 0; j < 64; ++j) 
-				{
-					//If the current page being checked is larger than the amount of available pages, break
-					if (i * 64 + j >= vertex_frame_count)
-					{
-						break;
-					}
-
-					//Set bit corresponding to page being checked in temporary variable
-					std::uint64_t to_test = 1Ui64 << j;
-					
-					//Run an 'and' against the temporary variable to see if the page is available
-					if ((m_vertex_buffer_bitmap[i] & to_test)) 
-					{
-						//Is this the first free page found?
-						if (counting) 
-						{
-							//Not the first free page, so increment the counter of consecutive free pages
-							free_frames++;
-							//Have we found the nessecary amount of free pages? If so, indicate that and break
-							if (free_frames == vertex_needed_frames)
-							{
-								found = true;
-								break;
-							}
-						}
-						else 
-						{
-							//This is the first free page
-							if (vertex_needed_frames == 1)
-							{
-								//We only need a single page, so store the number of the page and break
-								found = true;
-								vertex_start_frame = i * 64 + j;
-								break;
-							}
-							else 
-							{
-								//We need multiple pages, so store the number of this page as the start page and continue
-								vertex_start_frame = i * 64 + j;
-								counting = true;
-								free_frames = 1;
-							}
-						}
-					}
-					else
-					{
-						//The page wasn't free, so stop counting.
-						counting = 0;
-						free_frames = 0;
-					}
-				}
-			}
-			else
-			{
-				//All 64 pages are occupied, so stop counting
-				counting = false;
-				free_frames = 0;
-			}
-
-			//Have we found enough free pages? If so, break
-			if (found == true)
-			{
-				break;
-			}
-		}
-
-		//The loop has exited, see if we've found enough free pages
-		if (found == false) 
+		// Check if we found a page.
+		if (!vertex_start_frame.has_value())
 		{
 			//We haven't found enough pages, so delete the mesh and return a nullptr
 			delete mesh;
 			return nullptr;
 		}
-		
+
 
 		//Repeat the same procedure as before, but now for the index buffer
 
 		auto index_frame_count = m_index_buffer_size / 65536;
 		auto index_needed_frames = SizeAlign(num_indices*index_size, 65536) / 65536;
 
-		std::uint64_t index_start_frame = 0;
-		counting = false;
-		found = false;
-		free_frames = 0;
+		auto index_start_frame = internal::FindFreePage(m_index_buffer_bitmap, index_frame_count, index_needed_frames);
 
-		for (std::uint64_t i = 0; i < m_index_buffer_bitmap.size(); ++i)
-		{
-			if (m_index_buffer_bitmap[i] != 0Ui64) 
-			{
-				for (std::uint64_t j = 0; j < 64; ++j) 
-				{
-					std::uint64_t to_test = 1Ui64 << j;
-					if (i * 64 + j >= index_frame_count)
-						break;
-
-					if ((m_index_buffer_bitmap[i] & to_test)) 
-					{
-						if (counting)
-						{
-							free_frames++;
-							if (free_frames == index_needed_frames) 
-							{
-								found = true;
-								break;
-							}
-						}
-						else
-						{
-							if (index_needed_frames == 1)
-							{
-								found = true;
-								index_start_frame = i * 64 + j;
-								break;
-							}
-							else 
-							{
-								index_start_frame = i * 64 + j;
-								counting = true;
-								free_frames = 1;
-							}
-						}
-					}
-					else 
-					{
-						counting = 0;
-						free_frames = 0;
-					}
-				}
-			}
-			else 
-			{
-				counting = false;
-				free_frames = 0;
-			}
-			if (found == true) 
-			{
-				break;
-			}
-		}
-
-		if (found == false) 
+		if (!index_start_frame.has_value())
 		{
 			delete mesh;
 			return nullptr;
@@ -340,17 +293,17 @@ namespace wr
 		//Mark all pages occupied by the new vertex buffer
 		for (std::uint64_t i = 0; i < vertex_needed_frames; ++i)
 		{
-			internal::ClearPage(&(m_vertex_buffer_bitmap), vertex_start_frame + i);
+			internal::ClearPage(m_vertex_buffer_bitmap, vertex_start_frame.value() + i);
 		}
 
 		//Mark all pages occupied by the new index buffer
 		for (std::uint64_t i = 0; i < vertex_needed_frames; ++i)
 		{
-			internal::ClearPage(&(m_index_buffer_bitmap), index_start_frame + i);
+			internal::ClearPage(m_index_buffer_bitmap, index_start_frame.value() + i);
 		}
 
 		//Store the offset of the allocated memory from the start of the staging buffer
-		mesh->m_vertex_staging_buffer_offset = vertex_start_frame * 65536;
+		mesh->m_vertex_staging_buffer_offset = vertex_start_frame.value() * 65536;
 		mesh->m_vertex_staging_buffer_size = num_vertices * vertex_size;
 		mesh->m_vertex_staging_buffer_stride = vertex_size;
 		mesh->m_vertex_count = num_vertices;
@@ -361,7 +314,7 @@ namespace wr
 		d3d12::UpdateStagingBuffer(m_vertex_buffer, vertices_data, num_vertices*vertex_size, mesh->m_vertex_staging_buffer_offset);
 
 		//Store the offset of the allocated memory from the start of the staging buffer
-		mesh->m_index_staging_buffer_offset = index_start_frame * 65536;
+		mesh->m_index_staging_buffer_offset = index_start_frame.value() * 65536;
 		mesh->m_index_staging_buffer_size = num_indices * index_size;
 		mesh->m_index_count = num_indices;
 
@@ -391,88 +344,11 @@ namespace wr
 		//Get amount of needed memory pages
 		auto vertex_needed_frames = SizeAlign(num_vertices*vertex_size, 65536) / 65536;
 
-		std::uint64_t vertex_start_frame = 0;
-		bool counting = false;
-		bool found = false;
-		int free_frames = 0;
-
-		//Go through all all pages to find free ones.
-		//Because we're storing the data in a single bit we can compare an entire int64 at once to speed things up
-		for (std::uint64_t i = 0; i < m_vertex_buffer_bitmap.size(); ++i)
-		{
-			//Check 64 pages at once
-			if (m_vertex_buffer_bitmap[i] != 0Ui64)
-			{
-				//At least a single page is free, so check all 64 pages
-				for (std::uint64_t j = 0; j < 64; ++j)
-				{
-					//If the current page being checked is larger than the amount of available pages, break
-					if (i * 64 + j >= vertex_frame_count)
-					{
-						break;
-					}
-
-					//Set bit corresponding to page being checked in temporary variable
-					std::uint64_t to_test = 1Ui64 << j;
-
-					//Run an 'and' against the temporary variable to see if the page is available
-					if ((m_vertex_buffer_bitmap[i] & to_test))
-					{
-						//Is this the first free page found?
-						if (counting)
-						{
-							//Not the first free page, so increment the counter of consecutive free pages
-							free_frames++;
-							//Have we found the nessecary amount of free pages? If so, indicate that and break
-							if (free_frames == vertex_needed_frames)
-							{
-								found = true;
-								break;
-							}
-						}
-						else
-						{
-							//This is the first free page
-							if (vertex_needed_frames == 1)
-							{
-								//We only need a single page, so store the number of the page and break
-								found = true;
-								vertex_start_frame = i * 64 + j;
-								break;
-							}
-							else
-							{
-								//We need multiple pages, so store the number of this page as the start page and continue
-								vertex_start_frame = i * 64 + j;
-								counting = true;
-								free_frames = 1;
-							}
-						}
-					}
-					else
-					{
-						//The page wasn't free, so stop counting.
-						counting = 0;
-						free_frames = 0;
-					}
-				}
-			}
-			else
-			{
-				//All 64 pages are occupied, so stop counting
-				counting = false;
-				free_frames = 0;
-			}
-
-			//Have we found enough free pages? If so, break
-			if (found == true)
-			{
-				break;
-			}
-		}
+		// Find Free Page
+		auto vertex_start_frame = internal::FindFreePage(m_vertex_buffer_bitmap, vertex_frame_count, vertex_needed_frames);
 
 		//The loop has exited, see if we've found enough free pages
-		if (found == false)
+		if (!vertex_start_frame.has_value())
 		{
 			//We haven't found enough pages, so delete the mesh and return a nullptr
 			delete mesh;
@@ -480,13 +356,13 @@ namespace wr
 		}
 
 		//Mark the allocated pages as occupied
-		for (std::uint64_t i = 0; i < vertex_needed_frames; ++i) 
+		for (std::uint64_t i = 0; i < vertex_needed_frames; ++i)
 		{
-			internal::ClearPage(&(m_vertex_buffer_bitmap), vertex_start_frame + i);
+			internal::ClearPage(m_vertex_buffer_bitmap, vertex_start_frame.value() + i);
 		}
 
 		//Store the offset of the allocated memory from the start of the staging buffer
-		mesh->m_vertex_staging_buffer_offset = vertex_start_frame * 65536;
+		mesh->m_vertex_staging_buffer_offset = vertex_start_frame.value() * 65536;
 		mesh->m_vertex_staging_buffer_size = num_vertices * vertex_size;
 		mesh->m_vertex_staging_buffer_stride = vertex_size;
 		mesh->m_vertex_count = num_vertices;
@@ -506,9 +382,9 @@ namespace wr
 
 	void D3D12ModelPool::DestroyModel(Model * model)
 	{
-		for (int i = 0; i < model->m_meshes.size(); ++i) 
+		for (auto& mesh : model->m_meshes)
 		{
-			DestroyMesh(model->m_meshes[i]);
+			DestroyMesh(mesh);
 		}
 
 		//TODO: Destroy possible materials owned by model. Material might be used by multiple models, use ref counts?
@@ -526,24 +402,7 @@ namespace wr
 		if (mesh->m_model_pool != this)
 			return;
 
-		//Find where in the vector storing meshes the mesh is located
-		std::vector<Mesh*>::iterator it;
-		for (it = m_mesh_handles.begin(); it != m_mesh_handles.end(); ++it) 
-		{
-			if ((*it) == mesh) 
-			{
-				break;
-			}
-		}
-
-		//Check if the mesh was actually located
-		if (it == m_mesh_handles.end())
-		{
-			return;
-		}
-
-		//Remove the mesh from the vector
-		m_mesh_handles.erase(it);
+		m_mesh_handles.erase(std::remove(m_mesh_handles.begin(), m_mesh_handles.end(), mesh), m_mesh_handles.end());
 
 		D3D12Mesh* n_mesh = static_cast<D3D12Mesh*>(mesh);
 
@@ -555,11 +414,11 @@ namespace wr
 		//Mark all occupied pages as free
 		for (int i = 0; i < frame_count; ++i)
 		{
-			internal::SetPage(&(m_vertex_buffer_bitmap), frame + i);
+			internal::SetPage(m_vertex_buffer_bitmap, frame + i);
 		}
 
 		//Does the mesh also have an index buffer?
-		if (n_mesh->m_index_buffer_base_address != 0) 
+		if (n_mesh->m_index_buffer_base_address != 0)
 		{
 			//Find at which page in memory the mesh index data starts
 			std::uint64_t frame = n_mesh->m_index_staging_buffer_offset / 65536;
@@ -567,9 +426,9 @@ namespace wr
 			std::uint64_t frame_count = SizeAlign(n_mesh->m_index_staging_buffer_size, 65536) / 65536;
 
 			//Mark all occupied pages as free
-			for (int i = 0; i < frame_count; ++i) 
+			for (int i = 0; i < frame_count; ++i)
 			{
-				internal::SetPage(&(m_index_buffer_bitmap), frame + i);
+				internal::SetPage(m_index_buffer_bitmap, frame + i);
 			}
 
 		}
