1e80f2f93f52045e74099ec07cfcd8478ad60b87
BNiels <n@osomi.net>
2018-12-03 11:22:02 +0100

    Light nodes are now allocated directly into the light buffer; when a node is deallocated, the nodes besides it move light to the left. When a node is deallocated or allocated, it signals the first light, because it stores light count.

--- src/scene_graph/light_node.cpp
+++ src/scene_graph/light_node.cpp
@@ -3,71 +3,79 @@
 namespace wr
 {
 
-	LightNode::LightNode(LightType tid, DirectX::XMVECTOR col) : Node()
+	LightNode::LightNode(LightType tid, DirectX::XMVECTOR col) : Node(), m_light(&m_temp)
 	{
 		SetType(tid);
 		SetColor(col);
 	}
 
-	LightNode::LightNode(DirectX::XMVECTOR dir, DirectX::XMVECTOR col) : Node()
+	LightNode::LightNode(DirectX::XMVECTOR dir, DirectX::XMVECTOR col) : Node(), m_light(&m_temp)
 	{
 		SetDirectional(dir, col);
 	}
 
-	LightNode::LightNode(DirectX::XMVECTOR pos, float rad, DirectX::XMVECTOR col) : Node()
+	LightNode::LightNode(DirectX::XMVECTOR pos, float rad, DirectX::XMVECTOR col) : Node(), m_light(&m_temp)
 	{
 		SetPoint(pos, rad, col);
 	}
 
-	LightNode::LightNode(DirectX::XMVECTOR pos, float rad, DirectX::XMVECTOR dir, float ang, DirectX::XMVECTOR col) : Node()
+	LightNode::LightNode(DirectX::XMVECTOR pos, float rad, DirectX::XMVECTOR dir, float ang, DirectX::XMVECTOR col) : Node(), m_light(&m_temp)
 	{
 		SetSpot(pos, rad, dir, ang, col);
 	}
 
-	//Set angle in degrees
+	LightNode::LightNode(const LightNode& old)
+	{
+		m_temp = old.m_temp;
+		m_temp.tid &= 0x3;
+		m_light = &m_temp;
+	}
+
+	LightNode& LightNode::operator=(const LightNode& old)
+	{
+		m_temp = old.m_temp;
+		m_temp.tid &= 0x3;
+		m_light = &m_temp;
+		return *this;
+	}
+
 	void LightNode::SetAngle(float ang)
 	{
-		m_light.ang = ang / 180.f * 3.1415926535f;
+		m_light->ang = ang / 180.f * 3.1415926535f;
 		SignalChange();
 	}
 
-	//Set radius
 	void LightNode::SetRadius(float rad)
 	{
-		m_light.rad = rad;
+		m_light->rad = rad;
 		SignalChange();
 	}
 
-	//Set type
 	void LightNode::SetType(LightType tid)
 	{
-		m_light.tid = (uint32_t) tid;
+		m_light->tid = (uint32_t) tid;
 		SignalChange();
 	}
 
-	//Sets position
 	void LightNode::SetPosition(DirectX::XMVECTOR pos)
 	{
-		memcpy(&m_light.pos, &pos, 12);
+		memcpy(&m_light->pos, &pos, 12);
 		SignalChange();
 	}
 
-	//Sets direction
 	void LightNode::SetDirection(DirectX::XMVECTOR dir)
 	{
 		dir = DirectX::XMVector3Normalize(dir);
-		memcpy(&m_light.dir, &dir, 12);
+		memcpy(&m_light->dir, &dir, 12);
 		SignalChange();
 	}
 
-	//Sets color
 	void LightNode::SetColor(DirectX::XMVECTOR col)
 	{
-		memcpy(&m_light.col, &col, 12);
+		memcpy(&m_light->col, &col, 12);
 		SignalChange();
 	}
 
-	//Set data for a directional light
 	void LightNode::SetDirectional(DirectX::XMVECTOR dir, DirectX::XMVECTOR col) 
 	{
 		SetType(LightType::DIRECTIONAL);
@@ -75,7 +83,6 @@ namespace wr
 		SetColor(col);
 	}
 
-	//Set data for a point light
 	void LightNode::SetPoint(DirectX::XMVECTOR pos, float rad, DirectX::XMVECTOR col)
 	{
 		SetType(LightType::POINT);
@@ -84,7 +91,6 @@ namespace wr
 		SetColor(col);
 	}
 
-	//Set data for a spot light
 	void LightNode::SetSpot(DirectX::XMVECTOR pos, float rad, DirectX::XMVECTOR dir, float ang, DirectX::XMVECTOR col)
 	{
 		SetType(LightType::POINT);
@@ -93,6 +99,11 @@ namespace wr
 		SetDirection(dir);
 		SetAngle(ang);
 		SetColor(col);
+	}
+
+	LightType LightNode::GetType()
+	{
+		return (LightType)(m_light->tid & 0x3);
 	}
 
 }
