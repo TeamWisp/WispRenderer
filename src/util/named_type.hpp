6477c87099a824a6021d58e057dd63c89c8b45b3
Viktor Zoutman <viktorzoutman@vzout.com>
2018-12-15 19:29:20 +0100

    Improved the named type and removed .get from places where it was unnessessary

--- src/util/named_type.hpp
+++ src/util/named_type.hpp
@@ -1,7 +1,46 @@
-#ifndef NAMED_TYPE_HPP
-#define NAMED_TYPE_HPP
+#pragma once
 
-#include "named_type_impl.hpp"
-#include "underlying_functionalities.hpp"
+#include <type_traits>
 
+// Enable empty base class optimization with multiple inheritance on Visual Studio.
+#if defined(_MSC_VER) && _MSC_VER >= 1910
+#  define FLUENT_EBCO __declspec(empty_bases)
+#else
+#  define FLUENT_EBCO
 #endif
+
+namespace fluent
+{
+
+template <typename T, typename Parameter, template<typename> class... Skills>
+class FLUENT_EBCO NamedType : public Skills<NamedType<T, Parameter, Skills...>>...
+{
+public:
+    using UnderlyingType = T;
+    
+    explicit constexpr NamedType(T const& value) : m_value(value) {}
+    
+    constexpr T& Get() { return m_value; }
+    constexpr T const& Get() const {return m_value; }
+
+	operator T&() { return m_value; }
+    
+    struct argument
+    {
+        template<typename U>
+        NamedType operator=(U&& value) const
+        {
+            return NamedType(std::forward<U>(value));
+        }
+        argument() = default;
+        argument(argument const&) = delete;
+        argument(argument &&) = delete;
+        argument& operator=(argument const&) = delete;
+        argument& operator=(argument &&) = delete;
+    };
+    
+private:
+    T m_value;
+};
+
+} /* fluent */
