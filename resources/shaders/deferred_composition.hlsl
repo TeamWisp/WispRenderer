7a108ac8a61815d7f71bf0f74bbf45bc4f3fac43
Florian Schut <mail@florianschut.nl>
2018-12-17 17:04:23 +0100

    [WIP]: started adding PBR to shaders

--- resources/shaders/deferred_composition.hlsl
+++ resources/shaders/deferred_composition.hlsl
@@ -15,8 +15,8 @@ struct Light
 
 StructuredBuffer<Light> lights : register(t3);
 
-Texture2D gbuffer_albedo : register(t0);
-Texture2D gbuffer_normal : register(t1);
+Texture2D gbuffer_albedo_roughness : register(t0);
+Texture2D gbuffer_normal_metallic : register(t1);
 Texture2D gbuffer_depth : register(t2);
 RWTexture2D<float4> output : register(u0);
 SamplerState s0 : register(s0);
@@ -46,13 +46,14 @@ float3 world_to_view(float4 pos, float4x4 view) {
 	return vpos.xyz;
 }
 
-float3 shade_light(float3 vpos, float3 V, float3 albedo, float3 normal, Light light) 
+float3 shade_light(float3 vpos, float3 V, float3 albedo, float3 normal, float metallic, float roughness, Light light)
 {
 	uint tid = light.tid & 3;
 
 	// Light pos and view directon to view.
 	float3 light_vpos = mul(view, float4(light.pos, 1)).xyz;
 	float3 light_vdir = normalize(mul(view, float4(light.dir, 0))).xyz;
+	float3 light_vhalf = normalize(V + light_vdir);
 
 	//Light direction (constant with directional, position dependent with other)
 	float3 L = (lerp(light_vpos - vpos, light.dir, tid == light_type_directional));
@@ -69,13 +70,13 @@ float3 shade_light(float3 vpos, float3 V, float3 albedo, float3 normal, Light li
 	float attenuation = lerp(1.0f - smoothstep(0, light.rad, light_dist), 1, tid == light_type_directional);
 	float3 radiance = (light.col * spot_intensity) * attenuation;
 
-	float3 lighting = BRDF(L, V, normal, 0.001f, 0.999f, albedo, radiance, light.col);
+	float3 lighting = BRDF(L, V, normal, metallic, roughness, albedo, radiance, light.col);
 
 	return lighting;
 
 }
 
-float3 shade_pixel(float3 vpos, float3 V, float3 albedo, float3 normal)
+float3 shade_pixel(float3 vpos, float3 V, float3 albedo, float metallic, float roughness, float3 normal)
 {
 
 	uint light_count = lights[0].tid >> 2;	//Light count is stored in 30 upper-bits of first light
@@ -85,7 +86,7 @@ float3 shade_pixel(float3 vpos, float3 V, float3 albedo, float3 normal)
 
 	for (uint i = 0; i < light_count; i++)
 	{
-		res += shade_light(vpos, V, albedo, normal, lights[i]);
+		res += shade_light(vpos, V, albedo, normal, metallic, roughness, lights[i]);
 	}
 
 	return res * albedo;
@@ -102,8 +103,11 @@ void main_cs(int3 dispatch_thread_id : SV_DispatchThreadID)
 	float2 screen_coord = int2(dispatch_thread_id.x, screen_size.y - dispatch_thread_id.y);
 
 	// GBuffer contents
-	const float3 albedo = gbuffer_albedo[screen_coord].xyz;
-	const float3 normal = gbuffer_normal[screen_coord].xyz;
+	const float3 albedo = gbuffer_albedo_roughness[screen_coord].xyz;
+	const float roughness = gbuffer_albedo_roughness[screen_coord].w;
+	const float3 normal = gbuffer_normal_metallic[screen_coord].xyz;
+	const float metallic = gbuffer_normal_metallic[screen_coord].w;
+	
 	const float depth_f = gbuffer_depth[screen_coord].r;
 
 	// View position and camera position
@@ -111,5 +115,5 @@ void main_cs(int3 dispatch_thread_id : SV_DispatchThreadID)
 	float3 V = normalize(-vpos);
 
 	//Do shading
-	output[int2(dispatch_thread_id.xy)] = float4(shade_pixel(vpos, V, albedo, normal), 1.f);
+	output[int2(dispatch_thread_id.xy)] = float4(shade_pixel(vpos, V, albedo, metallic, roughness, normal), 1.f);
 }
