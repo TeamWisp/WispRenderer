fe11976f2ad79fcdd9c660fb907a6f4a17619c79
Jens Hagen <hagen-j@outlook.com>
2018-12-17 13:25:00 +0100

    [change] set local

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @Echo off
 
 REM ##### COLOR SUPPORT #####
-SETLOCAL EnableDelayedExpansion
+SETLOCAL ENABLEDELAYEDEXPANSION
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
   set "DEL=%%a"
 )
