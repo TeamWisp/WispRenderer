ac56020ecff4b41135e0b5d385209ef3ce072fab
Florian Schut <mail@florianschut.nl>
2019-01-08 13:43:31 +0100

    [EDIT]: Moved lighting code to new HLSL file

--- resources/shaders/deferred_composition.hlsl
+++ resources/shaders/deferred_composition.hlsl
@@ -1,24 +1,12 @@
 #include "fullscreen_quad.hlsl"
 #include "pbr_util.hlsl"
 #include "hdr_util.hlsl"
-
-struct Light 
-{
-	float3 pos;			//Position in world space for spot & point
-	float rad;			//Radius for point, height for spot
-
-	float3 col;			//Color
-	uint tid;			//Type id; light_type_x
-
-	float3 dir;			//Direction for spot & directional
-	float ang;			//Angle for spot; in radians
-};
-
-StructuredBuffer<Light> lights : register(t3);
+#include "lighting.hlsl"
 
 Texture2D gbuffer_albedo_roughness : register(t0);
 Texture2D gbuffer_normal_metallic : register(t1);
 Texture2D gbuffer_depth : register(t2);
+
 RWTexture2D<float4> output : register(u0);
 SamplerState s0 : register(s0);
 
@@ -29,10 +17,6 @@ cbuffer CameraProperties : register(b0)
 	float4x4 inv_projection;
 };
 
-static uint light_type_point = 0;
-static uint light_type_directional = 1;
-static uint light_type_spot = 2;
-
 static uint min_depth = 0xFFFFFFFF;
 static uint max_depth = 0x0;
 
@@ -40,46 +24,6 @@ float3 unpack_position(float2 uv, float depth, float4x4 proj_inv, float4x4 view_
 	const float4 ndc = float4(uv * 2.0 - 1.0, depth, 1.0);
 	const float4 pos = mul( view_inv, mul(proj_inv, ndc));
 	return (pos / pos.w).xyz;
-}
-
-float3 shade_light(float3 pos, float3 V, float3 albedo, float3 normal, float metallic, float roughness, Light light)
-{
-	uint tid = light.tid & 3;
-
-	//Light direction (constant with directional, position dependent with other)
-	float3 L = (lerp(light.pos - pos, light.dir, tid == light_type_directional));
-	float light_dist = length(L);
-	L /= light_dist;
-
-	//Spot intensity (only used with spot; but always calculated)
-	float min_cos = cos(light.ang);
-	float max_cos = lerp(min_cos, 1, 0.5f);
-	float cos_angle = dot(light.dir, -L);
-	float spot_intensity = lerp(smoothstep(min_cos, max_cos, cos_angle), 1, tid != light_type_spot);
-
-	//Attenuation & spot intensity (only used with point or spot)
-	float attenuation = lerp(1.0f - smoothstep(0, light.rad, light_dist), 1, tid == light_type_directional);
-	float3 radiance = (light.col * spot_intensity) * attenuation;
-
-	float3 lighting = BRDF(L, V, normal, metallic, roughness, albedo, radiance, light.col);
-
-	return lighting;
-
-}
-
-float3 shade_pixel(float3 pos, float3 V, float3 albedo, float metallic, float roughness, float3 normal)
-{
-	uint light_count = lights[0].tid >> 2;	//Light count is stored in 30 upper-bits of first light
-
-	float ambient = 0.1f;
-	float3 res = float3(ambient, ambient, ambient);
-
-	for (uint i = 0; i < light_count; i++)
-	{
-		res += shade_light(pos, V, albedo, normal, metallic, roughness, lights[i]);
-	}
-
-	return res * albedo;
 }
 
 [numthreads(16, 16, 1)]
---
+++ resources/shaders/lighting.hlsl
@@ -0,0 +1,57 @@
+struct Light 
+{
+	float3 pos;			//Position in world space for spot & point
+	float rad;			//Radius for point, height for spot
+
+	float3 col;			//Color
+	uint tid;			//Type id; light_type_x
+
+	float3 dir;			//Direction for spot & directional
+	float ang;			//Angle for spot; in radians
+};
+
+StructuredBuffer<Light> lights : register(t3);
+
+static uint light_type_point = 0;
+static uint light_type_directional = 1;
+static uint light_type_spot = 2;
+
+float3 shade_light(float3 pos, float3 V, float3 albedo, float3 normal, float metallic, float roughness, Light light)
+{
+	uint tid = light.tid & 3;
+
+	//Light direction (constant with directional, position dependent with other)
+	float3 L = (lerp(light.pos - pos, light.dir, tid == light_type_directional));
+	float light_dist = length(L);
+	L /= light_dist;
+
+	//Spot intensity (only used with spot; but always calculated)
+	float min_cos = cos(light.ang);
+	float max_cos = lerp(min_cos, 1, 0.5f);
+	float cos_angle = dot(light.dir, -L);
+	float spot_intensity = lerp(smoothstep(min_cos, max_cos, cos_angle), 1, tid != light_type_spot);
+
+	//Attenuation & spot intensity (only used with point or spot)
+	float attenuation = lerp(1.0f - smoothstep(0, light.rad, light_dist), 1, tid == light_type_directional);
+	float3 radiance = (light.col * spot_intensity) * attenuation;
+
+	float3 lighting = BRDF(L, V, normal, metallic, roughness, albedo, radiance, light.col);
+
+	return lighting;
+
+}
+
+float3 shade_pixel(float3 pos, float3 V, float3 albedo, float metallic, float roughness, float3 normal)
+{
+	uint light_count = lights[0].tid >> 2;	//Light count is stored in 30 upper-bits of first light
+
+	float ambient = 0.1f;
+	float3 res = float3(ambient, ambient, ambient);
+
+	for (uint i = 0; i < light_count; i++)
+	{
+		res += shade_light(pos, V, albedo, normal, metallic, roughness, lights[i]);
+	}
+
+	return res * albedo;
+}
