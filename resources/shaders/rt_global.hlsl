54c2342373c3e5c99d2b2413e58172b85555e202
VZout <bideblade1@hotmail.com>
2019-02-25 09:32:32 +0100

    Can't remember

--- resources/shaders/rt_global.hlsl
+++ resources/shaders/rt_global.hlsl
@@ -1,10 +1,10 @@
-#define MAX_RECURSION 3
+#define MAX_RECURSION 2
 //#define FOUR_X_AA
 //#define PATH_TRACING
 //#define DEPTH_OF_FIELD
 #define EPSILON 0.01
-//#define SOFT_SHADOWS
-#define MAX_SHADOW_SAMPLES 4
+#define SOFT_SHADOWS
+#define MAX_SHADOW_SAMPLES 1
 
 RaytracingAccelerationStructure Scene : register(t0, space0);
 
