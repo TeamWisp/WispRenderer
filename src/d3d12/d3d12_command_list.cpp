712ff009e6cf7507498a184bdf1860da021ff72f
Viktor Zoutman <bideblade1@hotmail.com>
2019-04-14 20:17:22 +0200

    Now using GraphicsCommandList4 instead of 5

--- src/d3d12/d3d12_command_list.cpp
+++ src/d3d12/d3d12_command_list.cpp
@@ -471,28 +471,9 @@ namespace wr::d3d12
 		cmd_list->m_native->ResourceBarrier(1, &barrier);
 	}
 
-	void UAVBarrier(CommandList* cmd_list, std::vector<TextureResource*> const & resources)
+	void UAVBarrier(CommandList* cmd_list, TextureResource* resource, unsigned int number_of_barriers)
 	{
-		std::vector<CD3DX12_RESOURCE_BARRIER> barriers;
-		barriers.reserve(resources.size());
-		for (auto& resource : resources)
-		{
-			barriers.emplace_back(CD3DX12_RESOURCE_BARRIER::UAV(resource->m_resource));
-		}
-
-		cmd_list->m_native->ResourceBarrier(barriers.size(), barriers.data());
-	}
-
-	void UAVBarrier(CommandList* cmd_list, std::vector<ID3D12Resource*> const & resources)
-	{
-		std::vector<CD3DX12_RESOURCE_BARRIER> barriers;
-		barriers.reserve(resources.size());
-		for (auto& resource : resources)
-		{
-			barriers.emplace_back(CD3DX12_RESOURCE_BARRIER::UAV(resource));
-		}
-
-		cmd_list->m_native->ResourceBarrier(barriers.size(), barriers.data());
+		cmd_list->m_native->ResourceBarrier(number_of_barriers, &CD3DX12_RESOURCE_BARRIER::UAV(resource->m_resource));
 	}
 
 	void Alias(CommandList* cmd_list, TextureResource* resource_before, TextureResource* resource_after)
