2bdb6be960862a51009f838260c2cb4b03d22c0d
Viktor Zoutman <bideblade1@hotmail.com>
2019-04-15 21:22:24 +0200

    Fixed some minor issues with ansel and added settings

--- src/render_tasks/d3d12_ansel.hpp
+++ src/render_tasks/d3d12_ansel.hpp
@@ -15,34 +15,65 @@ namespace wr
 {
 	struct AnselSettings
 	{
-		struct Runtime
+		struct Setup
 		{
+			float m_meters_in_world_unit = 0.5f; // Game scale, the size of a world unit measured in meters. Viktor: Honestly I don't get this value. All speeds are set in unit space not meters...
 		};
 
+		struct Runtime
+		{
+			bool m_allow_translation = true; // User can move the camera during session
+			bool m_allow_rotation = true; // Camera can be rotated during session
+			bool m_allow_fov = true; // FoV can be modified during session
+			bool m_allow_mono_360 = true; // Game allows 360 capture during session
+			bool m_allow_stero_360 = true; // Game allows 360 stereo capture during session
+			bool m_allow_raw = false; // Game allows capturing pre-tonemapping raw HDR buffer
+			bool m_allow_pause = true; // Game is paused during capture
+			bool m_allow_highres = true; // Game allows highres capture during session
+			float m_translation_speed_in_world_units_per_sec = 2.f; // The speed at which camera moves in the world, initialized with a value given in Configuration
+			float m_rotation_speed_in_deg_per_second = 90.f; // The speed at which camera rotates, initialized with a value given in Configuration
+			float m_maximum_fov_in_deg = 179; // The maximum FoV value in degrees displayed in the Ansel UI. Any value in the range [140, 179] can be specified and values outside will be clamped to this range.
+		};
+
+		Setup m_setup;
 		Runtime m_runtime;
 	};
 
 	struct AnselData
 	{
+		bool out_ansel_support;
 #ifdef NVIDIA_GAMEWORKS_ANSEL
-		std::optional<ansel::Camera> out_original_camera;
+		std::optional<ansel::Camera> out_original_camera; // Used to restore the camera to the original settings when exiting Ansel.
 #endif
 	};
 
+	// Static variables so we can access them from the callbacks.
+	static AnselSettings ansel_settings = AnselSettings();
 	static bool ansel_session_running = false;
 
 	namespace internal
 	{
 #ifdef NVIDIA_GAMEWORKS_ANSEL
+		auto fill_ansel_config_obj = [](auto & conf)
+		{
+			conf.isTranslationAllowed = ansel_settings.m_runtime.m_allow_translation;
+			conf.isRotationAllowed = ansel_settings.m_runtime.m_allow_rotation;
+			conf.isFovChangeAllowed = ansel_settings.m_runtime.m_allow_fov;
+			conf.is360MonoAllowed = ansel_settings.m_runtime.m_allow_mono_360;
+			conf.is360StereoAllowed = ansel_settings.m_runtime.m_allow_stero_360;
+			conf.isHighresAllowed = ansel_settings.m_runtime.m_allow_highres;
+			conf.isRawAllowed = ansel_settings.m_runtime.m_allow_raw;
+			conf.isPauseAllowed = ansel_settings.m_runtime.m_allow_pause;
+			conf.translationalSpeedInWorldUnitsPerSecond = ansel_settings.m_runtime.m_translation_speed_in_world_units_per_sec;
+			conf.rotationalSpeedInDegreesPerSecond = ansel_settings.m_runtime.m_rotation_speed_in_deg_per_second;
+			conf.maximumFovInDegrees = ansel_settings.m_runtime.m_maximum_fov_in_deg;
+		};
+
 		ansel::StartSessionStatus startAnselSessionCallback(ansel::SessionConfiguration& conf, void* userPointer)
 		{
 			ansel_session_running = true;
 
-			conf.isTranslationAllowed = true;
-			conf.isRotationAllowed = true;
-			conf.isFovChangeAllowed = true;
-			conf.is360MonoAllowed = true;
-			conf.is360StereoAllowed = true;
+			fill_ansel_config_obj(conf);
 
 
 			return ansel::kAllowed;
@@ -73,6 +104,8 @@ namespace wr
 			ansel_camera.rotation = { camera->m_rotation.m128_f32[0], camera->m_rotation.m128_f32[1], camera->m_rotation.m128_f32[2], camera->m_rotation.m128_f32[3] };
 			ansel_camera.nearPlane = camera->m_frustum_near;
 			ansel_camera.farPlane = camera->m_frustum_far;
+			ansel_camera.projectionOffsetX = proj_offset.first;
+			ansel_camera.projectionOffsetY = proj_offset.second;
 
 			return ansel_camera;
 		}
@@ -83,6 +116,8 @@ namespace wr
 			camera->SetPosition({ ansel_camera.position.x, ansel_camera.position.y, ansel_camera.position.z });
 			camera->SetRotationQuaternion({ ansel_camera.rotation.x, ansel_camera.rotation.y , ansel_camera.rotation.z , ansel_camera.rotation.w });
 			camera->m_fov = wr::CameraNode::FoV(wr::CameraNode::FovDefault(ansel_camera.fov));
+			camera->m_frustum_near = ansel_camera.nearPlane;
+			camera->m_frustum_far = ansel_camera.farPlane;
 		}
 #endif
 
@@ -93,6 +128,7 @@ namespace wr
 			auto n_device = n_render_system.m_device;
 			auto& data = fg.GetData<AnselData>(handle);
 			auto n_render_target = fg.GetRenderTarget<d3d12::RenderTarget>(handle);
+			ansel_settings = fg.GetSettings<AnselSettings>(handle);
 
 			if (!n_render_system.m_window.has_value())
 			{
@@ -100,16 +136,18 @@ namespace wr
 				return;
 			}
 
-			if (!ansel::isAnselAvailable())
+			data.out_ansel_support = ansel::isAnselAvailable();
+			if (!data.out_ansel_support)
 			{
 				LOGW("Your machine doesn't seem to support NVIDIA Ansel");
+				return;
 			}
 
 			auto window = n_render_system.m_window.value();
 
 			ansel::Configuration config;
-			config.translationalSpeedInWorldUnitsPerSecond = 2.f;
-			config.metersInWorldUnit = 0.5;
+			config.translationalSpeedInWorldUnitsPerSecond = ansel_settings.m_runtime.m_translation_speed_in_world_units_per_sec;
+			config.metersInWorldUnit = ansel_settings.m_setup.m_meters_in_world_unit;
 			config.right = { 1, 0, 0 };
 			config.up = { 0, 1, 0 };
 			config.forward = { 0, 0, -1 };
@@ -141,7 +179,12 @@ namespace wr
 #ifdef NVIDIA_GAMEWORKS_ANSEL
 			auto& n_render_system = static_cast<D3D12RenderSystem&>(rs);
 			auto& data = fg.GetData<AnselData>(handle);
-			auto settings = fg.GetSettings<AnselSettings>(handle);
+			ansel_settings = fg.GetSettings<AnselSettings>(handle);
+
+			if (!data.out_ansel_support)
+			{
+				return;
+			}
 
 			if (!n_render_system.m_render_window.has_value())
 			{
@@ -162,7 +205,7 @@ namespace wr
 				ansel::updateCamera(ansel_camera);
 
 				AnselToNativeCamera(ansel_camera, camera);
-			} 
+			}
 			else if (data.out_original_camera.has_value())
 			{
 				auto camera = sg.GetActiveCamera();
@@ -222,7 +265,7 @@ namespace wr
 		desc.m_type = RenderTaskType::COMPUTE;
 		desc.m_allow_multithreading = true;
 
-		frame_graph.AddTask<AnselData>(desc, FG_DEPS(1, DeferredMainTaskData));
+		frame_graph.AddTask<AnselData>(desc);
 		frame_graph.UpdateSettings<AnselData>(AnselSettings());
 	}
 
