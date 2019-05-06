2c661ea8654236394a05fb63cf7a91fd4fd56db4
BNiels <n@osomi.net>
2019-05-06 15:47:52 +0200

    Made the ball not red again

--- demo/scene_viknell.hpp
+++ demo/scene_viknell.hpp
@@ -46,10 +46,10 @@ namespace viknell_scene
 			mirror_material = material_pool->Create(texture_pool.get());
 			wr::Material* mirror_internal = material_pool->GetMaterial(mirror_material);
 
-			float red[] = { 1, 0, 0 };
+			float not_red[] = { 1, 1, 1 };
 			mirror_internal->SetConstant(wr::MaterialConstantType::ROUGHNESS, 0);
 			mirror_internal->SetConstant(wr::MaterialConstantType::METALLIC, 1);
-			mirror_internal->SetConstant(wr::MaterialConstantType::COLOR, red);
+			mirror_internal->SetConstant(wr::MaterialConstantType::COLOR, not_red);
 
 			bamboo_material = material_pool->Create(texture_pool.get());
 			wr::Material* bamboo_material_internal = material_pool->GetMaterial(bamboo_material);
