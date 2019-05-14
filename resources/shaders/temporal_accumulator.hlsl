fd0e9c418dc06a28f1c4fa1732a10970455f67b5
Koen Buitenhuis <161563@edu.nhtv.nl>
2019-05-14 16:53:26 +0200

    [DELETED] Removed leftover file

--- resources/shaders/temporal_accumulator.hlsl
+++
@@ -1,6 +0,0 @@
-Texture2D<float4> noisy_data : register(t0);
-Texture2D<float4> accum : register(t1);
-RWTexture2D<float4> output   : register(u0);
-RWTexture2D<float4> variance : register(u1);
-SamplerState point_sampler   : register(s0);
-
