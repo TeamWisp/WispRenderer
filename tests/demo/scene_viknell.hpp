6816011de7cf9c1bf4aeb1361006e6841f936cbb
VZout <bideblade1@hotmail.com>
2019-05-20 14:05:50 +0200

    Reverted settings and viknell

--- tests/demo/scene_viknell.hpp
+++ tests/demo/scene_viknell.hpp
@@ -59,7 +59,7 @@ namespace viknell_scene
 			bamboo_material_internal->SetTexture(wr::TextureType::METALLIC, bamboo_metallic);
 
 			plane_model = model_pool->Load<wr::VertexColor>(material_pool.get(), texture_pool.get(), "resources/models/plane.fbx");
-			test_model = model_pool->LoadWithMaterials<wr::VertexColor>(material_pool.get(), texture_pool.get(), "resources/models/sponza/sponza.obj");
+			test_model = model_pool->LoadWithMaterials<wr::VertexColor>(material_pool.get(), texture_pool.get(), "resources/models/xbot.fbx");
 			sphere_model = model_pool->Load<wr::VertexColor>(material_pool.get(), texture_pool.get(), "resources/models/sphere.fbx");
 		}
 
@@ -90,16 +90,39 @@ namespace viknell_scene
 		auto skybox = scene_graph->CreateChild<wr::SkyboxNode>(nullptr, resources::equirectangular_environment_map);
 
 		// Geometry
+		auto floor = scene_graph->CreateChild<wr::MeshNode>(nullptr, resources::plane_model);
+		auto roof = scene_graph->CreateChild<wr::MeshNode>(nullptr, resources::plane_model);
+		auto back_wall = scene_graph->CreateChild<wr::MeshNode>(nullptr, resources::plane_model);
+		auto left_wall = scene_graph->CreateChild<wr::MeshNode>(nullptr, resources::plane_model);
+		auto right_wall = scene_graph->CreateChild<wr::MeshNode>(nullptr, resources::plane_model);
 		test_model = scene_graph->CreateChild<wr::MeshNode>(nullptr, resources::test_model);
+		auto sphere = scene_graph->CreateChild<wr::MeshNode>(nullptr, resources::sphere_model);
+		floor->SetPosition({0, -1, 0});
+		floor->SetRotation({90_deg, 0, 0});
+		floor->AddMaterial(resources::bamboo_material);
+		sphere->SetPosition({1, -1, -1});
+		sphere->SetScale({0.6f, 0.6f, 0.6f});
+		sphere->AddMaterial(resources::mirror_material);
+		roof->SetPosition({0, 1, 0});
+		roof->SetRotation({-90_deg, 0, 0});
+		roof->AddMaterial(resources::bamboo_material);
+		back_wall->SetPosition({0, 0, -1});
+		back_wall->SetRotation({0, 180_deg, 0});
+		back_wall->AddMaterial(resources::bamboo_material);
+		left_wall->SetPosition({-1, 0, 0});
+		left_wall->SetRotation({0, -90_deg, 0});
+		left_wall->AddMaterial(resources::bamboo_material);
+		right_wall->SetPosition({1, 0, 0});
+		right_wall->SetRotation({0, 90_deg, 0});
+		right_wall->AddMaterial(resources::bamboo_material);
 		test_model->SetPosition({0, -1, 0});
 		test_model->SetRotation({0, 180_deg, 0});
 		test_model->SetScale({0.01f,0.01f,0.01f});
 
 		// Lights
-		auto point_light_0 = scene_graph->CreateChild<wr::LightNode>(nullptr, wr::LightType::POINT, DirectX::XMVECTOR{ 2, 2, 2 });
+		auto point_light_0 = scene_graph->CreateChild<wr::LightNode>(nullptr, wr::LightType::DIRECTIONAL, DirectX::XMVECTOR{ 1, 1, 1 });
 		point_light_0->SetRotation({ 20.950f, 0.98f, 0.f });
-		point_light_0->SetPosition({ -0, 2, 0 });
-		point_light_0->SetRadius(10000);
+		point_light_0->SetPosition({ -0.002f, 0.080f, 1.404f });
 
 		auto point_light_1 = scene_graph->CreateChild<wr::LightNode>(nullptr, wr::LightType::POINT, DirectX::XMVECTOR{1, 0, 0});
 		point_light_1->SetRadius(5.0f);
