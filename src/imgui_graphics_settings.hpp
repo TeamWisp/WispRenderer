e355bba92b2fd0158ae9f783811eafdc23758eff
Florian Schut <mail@florianschut.nl>
2019-06-06 09:08:56 +0200

    Fixed indenting

--- src/imgui_graphics_settings.hpp
+++ src/imgui_graphics_settings.hpp
@@ -24,16 +24,16 @@ namespace wr::imgui::window
 
 			if (frame_graph->HasTask<wr::RTAOData>())
 			{
-			ImGui::LabelText("", "RTAO");
-			ImGui::Separator();
+				ImGui::LabelText("", "RTAO");
+				ImGui::Separator();
 			
 				ImGui::DragFloat("Bias", &rtao_user_settings.m_runtime.bias, 0.01f, 0.0f, 100.f);
 				ImGui::DragFloat("Radius", &rtao_user_settings.m_runtime.radius, 0.1f, 0.0f, 1000.f);
 				ImGui::DragFloat("Power", &rtao_user_settings.m_runtime.power, 0.1f, 0.0f, 10.f);
 				ImGui::DragInt("SPP", &rtao_user_settings.m_runtime.sample_count, 1, 0, 1073741824);
 
-					frame_graph->UpdateSettings<wr::RTAOData>(rtao_user_settings);
-			ImGui::Dummy(ImVec2(0.0f, 10.0f));
+				frame_graph->UpdateSettings<wr::RTAOData>(rtao_user_settings);
+				ImGui::Dummy(ImVec2(0.0f, 10.0f));
 			
 			}
 			
