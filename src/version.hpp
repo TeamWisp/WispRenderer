c84c65222dc1c9608d14fd8ef4041907f6df911b
Viktor Zoutman <bideblade1@hotmail.com>
2019-06-13 13:08:38 +0200

    Updated the license

--- src/version.hpp
+++ src/version.hpp
@@ -1,6 +1,6 @@
 /*!
 A constexpr way to obtain program version information.
-Copyright © 2018, Viktor Zoutman
+Copyright © 2019, Team Wisp
 
 Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
 
